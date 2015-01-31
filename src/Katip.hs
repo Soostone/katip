298cd0dba9db849ae18c5ec5ae7f9f567f4277bb
Ozgun Ataman <ozgun.ataman@soostone.com>
2015-01-31 17:24:26 -0500

    Expose a single base module for easy import

---
+++ src/Katip.hs
@@ -0,0 +1,9 @@
+module Katip
+    ( module Katip.Core
+    , module Katip.Scribes.Handle
+    ) where
+
+-------------------------------------------------------------------------------
+import           Katip.Core
+import           Katip.Scribes.Handle
+-------------------------------------------------------------------------------
