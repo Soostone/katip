b53d122cd21e075c14741c78870a2a62db91bb22
Michael Xavier <michael@michaelxavier.net>
2015-03-16 09:57:52 -0700

    Initial work on async buffer
    Need to decide what to do if a message can't be sent to the
    server. Should we drop it? Throw it back on the queue?

--- katip-elasticsearch/src/Katip/Scribes/ElasticSearch.hs
+++ katip-elasticsearch/src/Katip/Scribes/ElasticSearch.hs
@@ -1,8 +1,17 @@
-module Katip.Scribes.ElasticSearch where
+module Katip.Scribes.ElasticSearch
+    ( mkEsScribe
+    , mkDocId
+    , EsQueueSize
+    , mkEsQueueSize
+    ) where
 
 
 -------------------------------------------------------------------------------
+import           Control.Applicative
+import           Control.Concurrent.Async
+import           Control.Concurrent.STM
 import           Control.Monad
+import           Data.Aeson
 import           Data.UUID
 import           Database.Bloodhound
 import           System.Random
@@ -16,13 +25,18 @@ mkEsScribe
     :: Server
     -> IndexName
     -> MappingName
+    -> EsQueueSize
     -> Severity
     -> Verbosity
     -> IO Scribe
-mkEsScribe server ix mapping sev verb = return $ Scribe $ \ i -> do
-  did <- mkDocId
-  when (_itemSeverity i >= sev) $
-    void $ indexDocument server ix mapping (itemJson verb i) did
+mkEsScribe server ix mapping qs sev verb = do
+    q <- newTBQueueIO $ unEsQueueSize qs
+    worker <- startWorker server ix mapping q
+    link worker
+
+    return $ Scribe $ \ i ->
+      when (_itemSeverity i >= sev) $
+        void $ atomically $ tryWriteTBQueue q (itemJson verb i)
 
 
 -------------------------------------------------------------------------------
@@ -30,8 +44,44 @@ mkDocId :: IO DocId
 mkDocId = (DocId . toString) `fmap` randomIO
 
 
+-------------------------------------------------------------------------------
+newtype EsQueueSize = EsQueueSize {
+       unEsQueueSize :: Int
+     } deriving (Show, Eq, Ord, Bounded)
 
 
+mkEsQueueSize :: Int -> Maybe EsQueueSize
+mkEsQueueSize n
+  | n >= 0    = Just $ EsQueueSize n
+  | otherwise = Nothing
 
 
+-------------------------------------------------------------------------------
+startWorker
+    :: Server
+    -> IndexName
+    -> MappingName
+    -> TBQueue Value
+    -> IO (Async ())
+startWorker server ix mapping q = async $ forever $ do
+    v <- atomically $ readTBQueue q
+    res <- waitCatch =<< async (sendLog v)
+    when (isLeft res) $ inCaseOfEmergency
+  where
+    sendLog v = void $ indexDocument server ix mapping v =<< mkDocId
+    --TODO reenqueue? drop? would be nice to log but we don't have that context
+    inCaseOfEmergency = return ()
 
+
+-------------------------------------------------------------------------------
+tryWriteTBQueue :: TBQueue a -> a -> STM Bool
+tryWriteTBQueue q v = do
+    ok <- not <$> isFullTBQueue q
+    when ok $ writeTBQueue q v
+    return ok
+
+
+-------------------------------------------------------------------------------
+isLeft :: Either a b -> Bool
+isLeft (Left _) = True
+isLeft _        = False
