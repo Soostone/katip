7649bba5c5dab1eede77c99e62948c257eff32cd
Michael Xavier <michael@michaelxavier.net>
2015-03-17 11:49:55 -0700

    Update es to work with our fork of bloodhound

--- katip-elasticsearch/src/Katip/Scribes/ElasticSearch.hs
+++ katip-elasticsearch/src/Katip/Scribes/ElasticSearch.hs
@@ -5,6 +5,7 @@ module Katip.Scribes.ElasticSearch where
 import           Control.Monad
 import           Data.UUID
 import           Database.Bloodhound
+import           Network.HTTP.Client
 import           System.Random
 -------------------------------------------------------------------------------
 import           Katip.Core
@@ -13,16 +14,22 @@ import           Katip.Core
 
 -------------------------------------------------------------------------------
 mkEsScribe
-    :: Server
+    :: ManagerSettings
+    -> Server
     -> IndexName
     -> MappingName
     -> Severity
     -> Verbosity
     -> IO Scribe
-mkEsScribe server ix mapping sev verb = return $ Scribe $ \ i -> do
-  did <- mkDocId
-  when (_itemSeverity i >= sev) $
-    void $ indexDocument server ix mapping (itemJson verb i) did
+mkEsScribe ms server ix mapping sev verb =
+  withManager ms $ \mgr -> do
+    let env = BHEnv { bhServer = server
+                    , bhManager = mgr
+                    }
+    return $ Scribe $ \ i -> do
+      did <- mkDocId
+      when (_itemSeverity i >= sev) $
+        void $ runBH env $ indexDocument ix mapping (itemJson verb i) did
 
 
 -------------------------------------------------------------------------------
