c5954514c5fca4ef9533047285cdb8d2a21d3a3e
Leonid Onokhov <sopvop@gmail.com>
2016-10-26 23:08:47 +0300

    Faster time encoding for Handle Scribe
    This implements time encoding functions using low-level Data.Text
    interface.
    
    Encoding functions do not allocate anything except the result, and
    work really fast.
    
    The Handle Scribe benchmark improved almost 4x times. From 24us to
    6.1 us.
    
    This should probably be used in JSON encoding, at least in the
    toJSON part.
    
    The toEncoding part should use efficient implementation in Data.Aeson
    to avoid allocating Text or ByteString outside of Builder.

--- katip/test/Main.hs
+++ katip/test/Main.hs
@@ -4,6 +4,7 @@ module Main (main) where
 import           Test.Tasty
 -------------------------------------------------------------------------------
 import qualified Katip.Tests
+import qualified Katip.Tests.Format.Time
 import qualified Katip.Tests.Scribes.Handle
 -------------------------------------------------------------------------------
 
@@ -15,5 +16,6 @@ testSuite :: TestTree
 testSuite = testGroup "katip"
   [
     Katip.Tests.tests
+  , Katip.Tests.Format.Time.tests
   , Katip.Tests.Scribes.Handle.tests
   ]
