fd6fee6e7ad5c01614587a6751b4ad268ed31176
Michael Xavier <michael@michaelxavier.net>
2017-07-07 10:23:38 -0700

    Add derive data typeable

--- katip/test/Katip/Tests.hs
+++ katip/test/Katip/Tests.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE CPP                        #-}
+{-# LANGUAGE DeriveDataTypeable         #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE RecordWildCards            #-}
