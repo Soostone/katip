a845d4dbb793058bd1dd23767786c7ac827cf6bb
Robin Palotai <palotai.robin@gmail.com>
2021-10-24 22:51:35 +0200

    Fix typo

--- katip/src/Katip/Monadic.hs
+++ katip/src/Katip/Monadic.hs
@@ -287,8 +287,8 @@ logTM = [|logItemM (Just $(getLocTH))|]
 --
 -- Same consideration as `logLoc` applies.
 --
--- By default, ocation will be logged from the module that invokes 'logLocM'.
--- If you want to wrap 'logLocM' in a helper, wrap the entire helper with
+-- By default, location will be logged from the module that invokes 'logLocM'.
+-- If you want to use 'logLocM' in a helper, wrap the entire helper in
 -- 'withFrozenCallStack' to retain the callsite of the helper in the logs.
 --
 -- This function does not require template-haskell. Using GHC <= 7.8 will result
