bc8510608db731866964ff13701041cfb08808f9
Robin Palotai <palotai.robin@gmail.com>
2021-10-24 22:49:41 +0200

    logLocM doc hint to retain callsite when wrapping
    For example, in pseudo-Haskell, this works with nice callsite:
    
    logOnException msg action = withFrozenCallStack $ do
       action `catch` (\e -> logLocM ErrorS (msg <> showLS e) >> throwIO e)

--- katip/src/Katip/Monadic.hs
+++ katip/src/Katip/Monadic.hs
@@ -287,8 +287,9 @@ logTM = [|logItemM (Just $(getLocTH))|]
 --
 -- Same consideration as `logLoc` applies.
 --
--- Location will be logged from the module that invokes 'logLocM' so
--- be aware that wrapping 'logLocM' will make location reporting useless.
+-- By default, ocation will be logged from the module that invokes 'logLocM'.
+-- If you want to wrap 'logLocM' in a helper, wrap the entire helper with
+-- 'withFrozenCallStack' to retain the callsite of the helper in the logs.
 --
 -- This function does not require template-haskell. Using GHC <= 7.8 will result
 -- in the emission of a log line without any location information.
